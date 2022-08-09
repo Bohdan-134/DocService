dc74a5c25b1b51de72a26c2ef037de9748ba6191
kBohdan <bkachmar1@gmail.com>
2022-08-09 17:59:52 +0300

    fix header

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,2 @@
 
-document.addEventListener("mousemove",parallax);function parallax(event){this.querySelectorAll(".header-main .bx").forEach((shift)=>{const position=shift.getAttribute("value");const x=(window.innerWidth-event.pageX*position)/120;const y=(window.innerHeight-event.pageY*position)/120;shift.style.transform=`translateX(${x}px) translateY(${y}px)`;});}
-window.addEventListener('deviceorientation',function(e){const x=Math.round(e.beta);const y=Math.round(e.gamma);const z=Math.round(e.alpha);document.querySelectorAll('.header-main .bx').forEach((item)=>{item.style.transform=`translateY(-${x}%)`;})});
+document.addEventListener("mousemove",parallax);function parallax(event){this.querySelectorAll(".bg-fixed .bx").forEach((shift)=>{const position=shift.getAttribute("value");const x=(window.innerWidth-event.pageX*position)/80;const y=(window.innerHeight-event.pageY*position)/80;shift.style.transform=`translateX(${x}px) translateY(${y}px)`;});}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,11 @@
 document.addEventListener("mousemove", parallax);
 
 function parallax(event) {
-    this.querySelectorAll(".header-main .bx").forEach((shift) => {
+    this.querySelectorAll(".bg-fixed .bx").forEach((shift) => {
         const position = shift.getAttribute("value");
-        const x = (window.innerWidth - event.pageX * position) / 120;
-        const y = (window.innerHeight - event.pageY * position) / 120;
+        const x = (window.innerWidth - event.pageX * position) / 80;
+        const y = (window.innerHeight - event.pageY * position) / 80;
 
         shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
     });
 }
-
-window.addEventListener('deviceorientation', function(e) {
-    const x = Math.round(e.beta);
-    const y = Math.round(e.gamma);
-    const z = Math.round(e.alpha);
-
-
-    //Parallax Effect
-
-    document.querySelectorAll('.header-main .bx').forEach((item) => {
-        item.style.transform = `translateY(-${x}%)`;
-    })
-
-});
